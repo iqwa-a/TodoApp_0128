eca5fe7bf7a9417a2b7ea68aebed129c4b6bd911
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-20 23:17:19 +0700

    Memperbaiki

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -95,12 +95,16 @@ import 'package:intl/intl.dart';
      );
    }
    void _validateSubmit() {
+      bool isValid = _key.currentState!.validate();
      setState(() {
        _dateError = _selectedDate == null ? 'Please select a date' : null;
        {
          addData();
        }
      });
+     if(isValid && _selectedDate != null){
+       addData();
+     }
    }
  
    @override
