00246f8be4a6920c5d38e39547cc10b0aa37e0dc
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-20 20:55:25 +0700

    menambahkab form key, controller, dan state

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -8,6 +8,12 @@ import 'package:flutter/material.dart';
  }
  
  class _FormPageState extends State<FormPage> {
+  final _key = GlobalKey<FormState>();
+   final TextEditingController _taskController = TextEditingController();
+   DateTime? _selectedDate;
+   String? _dateError;
+   List<Map<String, dynamic>> listTugas = [];
+ 
    @override
    Widget build(BuildContext context) {
      return const Placeholder();
