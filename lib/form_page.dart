cfccafb5312b2a5f75f4b4ee066e748d934239ad
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-20 21:31:42 +0700

    add fungsi untuk memilih tanggal dan waktu

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
  
  class FormPage extends StatefulWidget {
    const FormPage({super.key});
@@ -30,6 +31,66 @@ import 'package:flutter/material.dart';
          backgroundColor: Colors.green,
          duration: Duration(seconds: 2),
        ),
+     );
+   }
+   Future<void> _showDateTimePicker(BuildContext context) async {
+     DateTime tempSelectDate = _selectedDate ?? DateTime.now();
+     await showModalBottomSheet(
+       context: context,
+       shape: const RoundedRectangleBorder(
+         borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+       ),
+       builder: (BuildContext builder) {
+         return StatefulBuilder(
+           builder: (context, setStateModal) {
+             return Container(
+               padding: const EdgeInsets.all(16),
+               height: 350,
+               child: Column(
+                 children: [
+                   const Text(
+                     'Set Task Date & Time',
+                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                   ),
+                   const SizedBox(height: 10),
+                   Expanded(
+                     child: CupertinoDatePicker(
+                       mode: CupertinoDatePickerMode.dateAndTime,
+                       initialDateTime: tempSelectDate,
+                       minimumDate: DateTime(2000),
+                       maximumDate: DateTime(2100),
+                       use24hFormat: false,
+                       onDateTimeChanged: (DateTime newDate) {
+                         int hour =
+                             newDate.hour % 12 == 0 ? 12 : newDate.hour % 12;
+                         setStateModal(() {
+                           tempSelectDate = DateTime(
+                             newDate.year,
+                             newDate.month,
+                             newDate.day,
+                             hour,
+                             newDate.minute.clamp(0, 59),
+                           );
+                         });
+                       },
+                     ),
+                   ),
+                   ElevatedButton(
+                     onPressed: () {
+                       setState(() {
+                         _selectedDate = tempSelectDate;
+                         _dateError = null;
+                       });
+                       Navigator.pop(context);
+                     },
+                     child: Text('Select'),
+                   ),
+                 ],
+               ),
+             );
+           },
+         );
+       },
      );
    }
  
