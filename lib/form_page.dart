8c25218e6c4262b6ced5075f13c42a4aa7c2ca0f
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-20 20:58:05 +0700

    MenambahkanFungsiTambahData

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -13,6 +13,18 @@ import 'package:flutter/material.dart';
    DateTime? _selectedDate;
    String? _dateError;
    List<Map<String, dynamic>> listTugas = [];
+   void addData() {
+     setState(() {
+       listTugas.add({
+         'task': _taskController.text,
+         'done': false,
+         'deadline': _selectedDate,
+       });
+       _taskController.clear();
+       _selectedDate = null;
+       _dateError = null;
+     });
+   }
  
    @override
    Widget build(BuildContext context) {
